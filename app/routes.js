8d80e42f20919047c52f466a2db26bfaafd0336d
LJMas <lucy.massie@madetech.com>
2022-07-20 16:15:21 +0100

    Change copy and skip next page if user does not share documents

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,20 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+router.post('/share-documents-answer', function (req, res) {
+
+    // Make a variable and give it the value from 'how-many-balls'
+    var shareDocs = req.session.data['share-documents']
+  
+    // Check whether the variable matches a condition
+    if (shareDocs == "I do not want to share my documents"){
+      // Send user to next page
+      res.redirect('/check-answers')
+    } else {
+      // Send user to ineligible page
+      res.redirect('/what-will-happen-with-my-data')
+    }
+  
+  })
 
 module.exports = router
